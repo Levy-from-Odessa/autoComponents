7ffd5784d04fad6c72e72763083b03b9afce25ab
Levy_from_Odessa <savelik12@gmail.com>
2021-03-18 16:53:53 +0200

    published

---
+++ node_modules/vue-generate-auto/blueprints/store/full/index.js
@@ -0,0 +1,20 @@
+
+
+  import * as {{name}}Actions from './actions';
+  import {{name}}Mutations from './mutations ';
+
+  const {{name}}Getters = {
+
+  }
+
+  const {{name}}State = () => ({
+
+  })
+  
+  export default {
+    namespace: true,
+    state: {{name}}State,
+    getters: {{name}}Getters ,
+    actions: {{name}}Actions,
+    mutations: {{name}}Mutations
+  };
